662eaad1a4e1b73b9c67f8923eda59839ff4df70
guidetuanhp <guidetuanhp@gmail.com>
2024-01-28 23:58:11 +0900

    new

---
+++ Maths/mathematics.py
@@ -0,0 +1,10 @@
+
+def multiplication(a, b):
+    result = a * b
+    return result
+def subtraction(a, b):
+    result = a - b
+    return result
+def summation(a, b):
+    result = a + b
+    return result
---
+++ server_request.py
@@ -0,0 +1,15 @@
+import requests
+url = "http://127.0.0.1:5000/person"
+data ={
+        "id": "4e1e61b4-8a27-11ed-a1eb-0242ac120002",
+        "first_name": "John",
+        "last_name": "Horne",
+        "graduation_year": 2001,
+        "address": "1 hill drive",
+        "city": "Atlanta",
+        "zip": "30339",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/139x100.png/cc0000/ffffff"
+}
+res = requests.post(url, json = data)
+print(res.text)
