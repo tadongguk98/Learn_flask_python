e110ad82fe78c6348bf4a08ba8e02265f7daa22f
guidetuanhp <guidetuanhp@gmail.com>
2024-01-29 00:34:30 +0900

    WSGI Server

--- server1.py
+++ server1.py
@@ -1,24 +1,24 @@
-from flask import Flask, make_response
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return "hello world"
-@app.route("/no_content")
-def no_content():
-    """return 'no content found' with a status of 204
-    Returns:
-        string: no content found with 204 status code
-    """
-    return ({"message":"No content found"}, 204)
-@app.route("/exp")
-def index_explicit():
-    """return 'Hello World' message with a status code of 200
-    Returns:
-        string: Hello World
-        status code: 200
-    """
-    resp = make_response({"message":"Hello World"})
-    resp.status_code = 200
-    return resp
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+# from flask import Flask, make_response
+# app = Flask(__name__)
+# @app.route("/")
+# def index():
+#     return "hello world"
+# @app.route("/no_content")
+# def no_content():
+#     """return 'no content found' with a status of 204
+#     Returns:
+#         string: no content found with 204 status code
+#     """
+#     return ({"message":"No content found"}, 204)
+# @app.route("/exp")
+# def index_explicit():
+#     """return 'Hello World' message with a status code of 200
+#     Returns:
+#         string: Hello World
+#         status code: 200
+#     """
+#     resp = make_response({"message":"Hello World"})
+#     resp.status_code = 200
+#     return resp
+# if __name__ == "__main__":
+#     app.run(debug=True)
